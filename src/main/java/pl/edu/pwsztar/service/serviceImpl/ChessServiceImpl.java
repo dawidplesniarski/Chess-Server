590283fa47f99ebfa97762e21818340a1338cb5a
dawidplesniarski <dawidplesniarski@gmail.com>
2020-05-07 17:04:38 +0200

    column to int mapper added. Switch case added

--- src/main/java/pl/edu/pwsztar/service/serviceImpl/ChessServiceImpl.java
+++ src/main/java/pl/edu/pwsztar/service/serviceImpl/ChessServiceImpl.java
@@ -7,21 +7,90 @@ import pl.edu.pwsztar.domain.chess.RulesOfGame;
 import pl.edu.pwsztar.domain.dto.FigureMoveDto;
 import pl.edu.pwsztar.service.ChessService;
 
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 @Service
 public class ChessServiceImpl implements ChessService {
 
     private RulesOfGame bishop;
     private RulesOfGame knight;
+    private RulesOfGame king;
+    private RulesOfGame queen;
+    private RulesOfGame rook;
+    private RulesOfGame pawn;
 
     @Autowired
     public ChessServiceImpl(@Qualifier("Bishop") RulesOfGame bishop,
-                            @Qualifier("Knight") RulesOfGame knight) {
+                            @Qualifier("Knight") RulesOfGame knight,
+                            @Qualifier("King") RulesOfGame king,
+                            @Qualifier("Queen") RulesOfGame queen,
+                            @Qualifier("Rock") RulesOfGame rook,
+                            @Qualifier("Pawn") RulesOfGame pawn) {
         this.bishop = bishop;
         this.knight = knight;
+        this.king = king;
+        this.queen = queen;
+        this.rook = rook;
+        this.pawn = pawn;
     }
 
     @Override
     public boolean isCorrectMove(FigureMoveDto figureMoveDto) {
-        return true;
+        int currentRow;
+        int newRow;
+        int currentCol;
+        int newCol;
+        List<String> startPoint = Arrays.asList(figureMoveDto.getStart().split("_"));
+        List<String> destinationPoint = Arrays.asList(figureMoveDto.getDestination().split("_"));
+
+        currentCol =  mapToInt(startPoint.get(0));
+        currentRow = Integer.parseInt(startPoint.get(1));
+        newCol = mapToInt(destinationPoint.get(0));
+        newRow = Integer.parseInt(destinationPoint.get(1));
+        System.out.println(figureMoveDto.getType());
+
+        switch(figureMoveDto.getType()){
+            case BISHOP: {
+                bishop.isCorrectMove(currentCol, currentRow, newCol, newRow);
+                break;
+            }
+            case KING: {
+                king.isCorrectMove(currentCol, currentRow, newCol, newRow);
+                break;
+            }
+            case QUEEN: {
+                queen.isCorrectMove(currentCol, currentRow, newCol, newRow);
+                break;
+            }
+            case PAWN: {
+                pawn.isCorrectMove(currentCol, currentRow, newCol, newRow);
+                break;
+            }
+            case ROCK: {
+                rook.isCorrectMove(currentCol, currentRow, newCol, newRow);
+                break;
+            }
+            case KNIGHT: {
+                knight.isCorrectMove(currentCol, currentRow, newCol, newRow);
+                break;
+            }
+        }
+        return false;
+    }
+    private Integer mapToInt(String key){
+        Map<String,Integer> columns = new LinkedHashMap<>();
+        columns.put("a",1);
+        columns.put("b",2);
+        columns.put("c",3);
+        columns.put("d",4);
+        columns.put("e",5);
+        columns.put("f",6);
+        columns.put("g",7);
+        columns.put("h",8);
+
+        return columns.get(key);
     }
 }
