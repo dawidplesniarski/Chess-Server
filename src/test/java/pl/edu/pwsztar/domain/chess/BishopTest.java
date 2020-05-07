848cf809c7914ff92b4a12264d03e84ed56947a0
dawidplesniarski <dawidplesniarski@gmail.com>
2020-05-07 16:17:59 +0200

    Knight tests added

--- src/test/java/pl/edu/pwsztar/domain/chess/BishopTest.java
+++ src/test/java/pl/edu/pwsztar/domain/chess/BishopTest.java
@@ -10,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class BishopTest {
 
     private RulesOfGame bishop = new RulesOfGame.Bishop();
+    private RulesOfGame knight = new RulesOfGame.Knight();
 
     @Tag("Bishop")
     @ParameterizedTest
@@ -25,10 +26,30 @@ class BishopTest {
 
     @ParameterizedTest
     @CsvSource({
-            "0,  1,  1,   -2",
+            "0,  1,  1, -2",
             "10, 10, 10,  10"
     })
     void checkIncorrectMoveForBishop(int xStart, int yStart, int xStop, int yStop) {
         assertFalse(bishop.isCorrectMove(xStart, yStart, xStop, yStop));
     }
+    @Tag("Knight")
+    @ParameterizedTest
+    @CsvSource({
+            "4, 4, 3, 6",
+            "5, 2, 6, 4",
+            "1, 3, 2, 5",
+            "2, 3, 1, 1"
+    })
+    void checkCorrectMoveForKnight(int xStart, int yStart, int xStop, int yStop) {
+        assertTrue(knight.isCorrectMove(xStart, yStart, xStop, yStop));
+    }
+    @ParameterizedTest
+    @CsvSource({
+            "4, 4, 4, 6",
+            "6, 3, 6, 1",
+    })
+    void checkIncorrectMoveForKnight(int xStart, int yStart, int xStop, int yStop) {
+        assertFalse(knight.isCorrectMove(xStart, yStart, xStop, yStop));
+    }
+
 }
